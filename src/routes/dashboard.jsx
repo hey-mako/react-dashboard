02ef9aeb3adf532245cc85c1a5e1510307f654bd
brh55 <brandonhim@live.com>
2018-06-10 16:21:30 -0700

    Remove top navs

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -1,10 +1,5 @@
 import DashboardPage from "views/Dashboard/Dashboard.jsx";
 import SettingsPage from "views/Settings/Settings.jsx";
-import TableList from "views/TableList/TableList.jsx";
-import Typography from "views/Typography/Typography.jsx";
-import Icons from "views/Icons/Icons.jsx";
-import Maps from "views/Maps/Maps.jsx";
-import NotificationsPage from "views/Notifications/Notifications.jsx";
 
 import {
   Dashboard,
